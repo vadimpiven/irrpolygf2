23ce2d7490e36926ad001dff6aa1a6b99f79d1b3
Vadim Piven <vadim@piven.tech>
2019-10-04 18:25:25 +0300

    documentation finished

--- docs/html/search/all_8.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['setirreducible_14',['setIrreducible',['../class_polynomial.html#a482dc393a21d745b0a00d98324c15707',1,'Polynomial']]]
-];
--- docs/html/search/all_9.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['Использование_15',['Использование',['../md___users_vadimpiven__development_berlekamp-zassenhaus_src__r_e_a_d_m_e.html',1,'']]]
-];
